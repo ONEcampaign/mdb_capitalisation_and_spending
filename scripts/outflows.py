13cccdc1b2e2fcbbd50faeae731e7a0918f39398
Jorge Rivera <jorge.rivera@one.org>
2024-04-03 12:18:24 +0200

    readme file and outputs

--- scripts/outflows.py
+++ scripts/outflows.py
@@ -2,6 +2,7 @@ import pandas as pd
 from oda_data import set_data_path, read_crs
 
 from scripts import config
+from scripts.logger import logger
 
 set_data_path(config.Paths.raw_data)
 
@@ -42,19 +43,12 @@ def map_grants_non_grants(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def summarise_outflows_year_type(df: pd.DataFrame) -> pd.DataFrame:
+def summarise_outflows_year_type(df: pd.DataFrame, grouper: list[str]) -> pd.DataFrame:
     """Summarise the outflows by year, donor and type of flow."""
     df = (
-        df.groupby(
-            [
-                "year",
-                "donor_name",
-                "flow_name",
-            ],
-            observed=True,
-            dropna=False,
-            as_index=False,
-        )[["usd_commitment", "usd_disbursement"]]
+        df.groupby(by=grouper, observed=True, dropna=False, as_index=False)[
+            ["usd_commitment", "usd_disbursement"]
+        ]
         .sum()
         .sort_values(["year", "usd_disbursement"], ascending=[False, False])
     )
@@ -62,18 +56,12 @@ def summarise_outflows_year_type(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def add_mdb_total(df: pd.DataFrame) -> pd.DataFrame:
+def add_mdb_total(df: pd.DataFrame, grouper: list[str]) -> pd.DataFrame:
     """Add a yearly total for the MDBs being studied."""
     df_total = (
-        df.groupby(
-            [
-                "year",
-                "flow_name",
-            ],
-            observed=True,
-            dropna=False,
-            as_index=False,
-        )[["usd_commitment", "usd_disbursement"]]
+        df.groupby(by=grouper, observed=True, dropna=False, as_index=False)[
+            ["usd_commitment", "usd_disbursement"]
+        ]
         .sum()
         .assign(donor_name="Total")
     )
@@ -83,18 +71,33 @@ def add_mdb_total(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def get_mdb_outflows(years: list[int] | range) -> pd.DataFrame:
+def export_mdb_outflows(years: list[int] | range) -> None:
     """Pipeline function to get the data"""
-    df = (
-        read_crs(years=years)
-        .pipe(keep_study_mdbs)
-        .pipe(map_grants_non_grants)
-        .pipe(add_mdb_total)
-        .pipe(summarise_outflows_year_type)
+
+    # get the data
+    logger.info("Reading the data. If it needs downloading, this may take a while.")
+    df = read_crs(years=years)
+
+    # process the data
+    logger.info("Processing the data")
+    df = df.pipe(keep_study_mdbs).pipe(map_grants_non_grants)
+
+    # Summarise the data by flow
+    df_by_flow = df.pipe(
+        add_mdb_total, grouper=["year", "donor_name", "flow_name"]
+    ).pipe(summarise_outflows_year_type, grouper=["year", "donor_name", "flow_name"])
+
+    df_summary = df_by_flow.pipe(add_mdb_total, grouper=["year", "donor_name"]).pipe(
+        summarise_outflows_year_type, grouper=["year", "donor_name"]
     )
 
-    return df
+    # Save the data
+    df_by_flow.to_csv(config.Paths.output / "mdb_outflows_by_flow.csv", index=False)
+    logger.info("Data saved to mdb_outflows_by_flow.csv")
+
+    df_summary.to_csv(config.Paths.output / "mdb_outflows_summary.csv", index=False)
+    logger.info("Data saved to mdb_outflows_summary.csv")
 
 
 if __name__ == "__main__":
-    data = get_mdb_outflows(years=range(2010, 2023))
+    export_mdb_outflows(years=range(2010, 2023))
